5577f9cf0074603b831270a133dc6c9d84679474
ccccmax <125292066+ccccmax@users.noreply.github.com>
2023-02-24 18:04:11 +0800

    Update matrix.h

--- Project4/matrix.h
+++ Project4/matrix.h
@@ -15,7 +15,7 @@ private:
 	int rows;
 	vector<vector<float>> data;
 public:
-	Matrix();
+	Matrix();//此段conststructor开始是我根据视频写的，但是运行不起来，这是查找网上资料后，写出来的的
 	Matrix(int cols, int rows);
 	Matrix(int cols, int rows, const vector<vector<float>>& data);
 	~Matrix()=default;
